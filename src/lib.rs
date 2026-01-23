33ac259871cc990ac1984f37c955e372feb38614
Syed Insaf <syedinsaf@proton.me>
2026-01-24 04:30:08 +0530

    refactor(proto): move generated protobuf bindings
    - Relocate chromeos_update_engine proto
    - From lib.rs to src/proto/chromeos_update_engine.rs
    -

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,3 @@
-// Generated protobuf bindings
-#[allow(clippy::all, dead_code)]
-pub(crate) mod chromeos_update_engine {
-    include!(concat!(env!("OUT_DIR"), "/chromeos_update_engine.rs"));
-}
-
 pub mod cmd;
 pub mod payload;
+pub mod proto;
